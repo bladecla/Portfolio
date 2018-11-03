a7aedf3a93a5ea9b6ca4aa69b614e2cf11d58980
bladecla <the.bkoku@gmail.com>
2018-11-03 01:04:37 -0500

    improved eye following

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
-const leftEye = document.getElementById("left-eye");
-const rightEye = document.getElementById("right-eye");
+//try to avoid declaring variables at 60fps
+const blake = document.getElementById("employable-hunk");
+const pt = blake.createSVGPoint();
+const anchor = document.getElementById("anchor");
 const eyes = Array.from(document.getElementsByClassName("eyes"));
-const minX = -7;
+const minX = -7; // eyeball boundaries
 const maxX = 7;
 const minY = -5;
 const maxY = 3;
 const eyeWidth = Math.abs(minX) + maxX;
 const eyeHeight = Math.abs(minY) + maxY;
 let px = 0, py = 0; 
-let h, w, yOffset;
+let h, w, yOffset, screenPt, mouseX, mouseY;
 
-document.addEventListener("mousemove", function(e){
+
+//(re)initialize anchor point for eye movement
+
+const getAnchorScreenCoordinates = function(){
+    pt.x = anchor.getAttribute("cx");
+    pt.y = anchor.getAttribute("cy");
+    return pt.matrixTransform(blake.getScreenCTM());
+}
+
+const setScreenPoint = function(){ screenPt = getAnchorScreenCoordinates(); }
+blake.onload = window.onresize = setScreenPoint;
+
+//calculate and apply eye transform
+const moveEyes = function(e){
     eyes.forEach(eye => {
         h = window.innerHeight;
         w = window.innerWidth;
         yOffset = h / 2;
-        px = (e.clientX / w) * eyeWidth - eyeWidth / 2;
-        py = (((e.clientY) / h) * eyeHeight - eyeHeight / 2);
-        console.log(py)
+        if(e){
+            mouseY = e.clientY;
+            mouseX = e.clientX;
+        }
+            px = (mouseX / w) * eyeWidth - (eyeWidth / 2);
+            py = (((mouseY - screenPt.y) / h) * eyeHeight);
+        // } else {
+        //     py = ((prevClientY - screenPt.y) / (h)) * eyeHeight;
+        // }
         eye.style.transform = `translate(${px}%, ${py}%)`
     });
-}, true);
+}
+
+document.onmousemove = moveEyes;
+window.onscroll = function(){
+    setScreenPoint();
+    moveEyes(null)
+}
