94718e250a055c2bd1996ee26936c40bbceb7de3
bladecla <the.bkoku@gmail.com>
2018-11-16 02:50:12 -0600

    basic smooth scroll

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
     const eyeWidth = 10, eyeHeight = 5;
     let px = 0, py = 0, screenPt, mouseX, mouseY; 
     let animIsPlaying = true;
-    let animationStack = {};
+    let animationStack = {}, scrollDuration = 500;
 
     // (re)initialize anchor point for animations
     const setAnchorScreenCoordinates = function(){
@@ -33,7 +33,7 @@
 
     // calculate and apply eye transform
     const moveEyes = function(e){
-    return function eyeLoop(){     
+    return function eyeStep(){     
             eyes.forEach(eye => {
                 if (animIsPlaying) setAnchorScreenCoordinates();
                 // always use last known mouse position 
@@ -54,9 +54,35 @@
     }
 
     // smooth scroll
+        // TODO: cap target scroll position at max scrollable position
+    const smoothScroll = function(e){
+        e.preventDefault()
+        let id = e.target.getAttribute("href");
+        let target = document.querySelector(id);
+        let startPosition = window.pageYOffset;
+        let targetPosition = target.getBoundingClientRect().top;
+        let amt, elapsed, startTime = null;
+        target.focus();
+        
+        return function scrollLoop(time){
+            if (startTime === null) startTime = time;
+            elapsed = time - startTime;
+            amt = easeInOutCubic(elapsed, startPosition, targetPosition, scrollDuration);
+            window.scrollTo(0, amt)
+            return elapsed < scrollDuration;
+        }
+        
+    };
+    
+    const easeInOutCubic = function (t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t*t + b;
+        t -= 2;
+        return c/2*(t*t*t + 2) + b;
+    };
+    
     Array.from(document.querySelectorAll(".navlink")).forEach(btn => {
-        let id = btn.getAttribute("href");
-
+        btn.onclick = e => scheduleAnimation(smoothScroll(e));
     });
 
     // schedule animations
@@ -72,24 +98,25 @@
     }
 
     // run animations
-    const loop = function(){
+        // animations should return true for loops
+    const loop = function(timestamp){
         if (animationStack) for (task in animationStack){
-            if (animationStack[task]()){
-
+            if (animationStack[task](timestamp)){
+                scheduleAnimation(animationStack[task])
             } else delete animationStack[task];
+            // console.log("ran ", task)
         }
-        // console.log("ran ", task)
     }
 
     ////* CSS animations *////
 
     // raise brows on navbar hover
-    const nav = document.getElementById("navbar");
+    const nav = document.querySelector("#navbar");
     nav.onmouseenter = nav.onmouseleave = () => document.getElementById("brows").classList.toggle("raised");;
 
     // populate sky with stars
     const starCount = 200;
-    const sky = document.getElementById("nightsky");
+    const sky = document.querySelector("#nightsky");
     const populateSky = function(){
         for(let i = 0; i < starCount; i++){
             let star = document.createElementNS("http://www.w3.org/2000/svg", "circle");
